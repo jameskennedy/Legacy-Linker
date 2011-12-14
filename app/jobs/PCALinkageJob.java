b777949ab163ec5124c8ef1cd81ba5e32c04bc65
James Kennedy <jkennedy@maketechnologies.com>
2011-12-14 12:48:35 -0800

    Updated PCALinkageJob such that it can only be executed by a single thread at a time.

--- app/jobs/PCALinkageJob.java
+++ app/jobs/PCALinkageJob.java
@@ -6,11 +6,25 @@ import services.PCALinkageService;
 
 public class PCALinkageJob extends Job {
 
+    private static Boolean inProgress = Boolean.FALSE;
+
     @Override
     public void doJob() {
-        Logger.info("START: Updating PCA links...");
-        int filesProcessed = PCALinkageService.updateFileLinkage();
-        Logger.info("STOP: Updated PCA links for %d files.", filesProcessed);
+        synchronized (inProgress) {
+            if (inProgress) {
+                Logger.info("SKIP: Skipped updating PCA links since job already in progress.");
+                return;
+            }
+            inProgress = Boolean.TRUE;
+        }
+
+        try {
+            Logger.info("START: Updating PCA links...");
+            int filesProcessed = PCALinkageService.updateFileLinkage();
+            Logger.info("STOP: Updated PCA links for %d files.", filesProcessed);
+        } finally {
+            inProgress = Boolean.FALSE;
+        }
     }
 
 }
